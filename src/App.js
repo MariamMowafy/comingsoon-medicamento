0223fc4ba22a0ca32eb5244dd461a115f731d5a8
Mariam Mowafy <mariammowafy1999@gmail.com>
2023-03-25 02:27:22 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from "./logotrns.png";
 import "./App.css";
-// import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { useMediaQuery } from "react-responsive";
 
 function App() {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
+  const isBigScreen = useMediaQuery({ query: "(min-width: 1824px)" });
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const isPortrait = useMediaQuery({ query: "(orientation: portrait)" });
+  const isRetina = useMediaQuery({ query: "(min-resolution: 2dppx)" });
+
   return (
-    <div class="body" id="root">
-      <div class="background">
+    <div className="body" id="root">
+      <div className="background">
         <span></span>
         <span></span>
         <span></span>
@@ -25,23 +33,16 @@ function App() {
         <img src={logo} alt="Logo" />
         <h1>Coming Soon</h1>
         <h3>Medicamento Site Is Nearly Ready</h3>
-        <div class="progress-bar">
+        <div className="progress-bar">
           <span></span>
         </div>
-        <div class="sm">
+        <div className="sm">
           <a href="https://www.facebook.com/Medicamento.eg/">
-            {/* <i class="fab fa-facebook-f"></i> */}
-            <i class="fab fa-facebook-f"></i>
+            <i className="fab fa-facebook-f"></i>
           </a>
           <a href="https://mail.google.com/mail/?view=cm&fs=1&to=kamalmahainy@gmail.com&su=SUBJECT&body=BODY&bcc=someone.else@example.com">
-            <i class="fa fa-envelope" aria-hidden="true"></i>
+            <i className="fa fa-envelope" aria-hidden="true"></i>
           </a>
-          {/* <a href="#">
-            <i class="fab fa-instagram"></i>
-          </a> */}
-          {/* <a href="#">
-            <i class="fab fa-twitter"></i>
-          </a> */}
         </div>
       </div>
     </div>
