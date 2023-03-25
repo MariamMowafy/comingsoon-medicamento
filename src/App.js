0c1e9f24ce8b0e97ee60edbe09f3bbb821b91b38
Mariam Mowafy <mariammowafy1999@gmail.com>
2023-03-25 02:19:54 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logotrns.png";
+import "./App.css";
+// import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="body" id="root">
+      <div class="background">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className="container">
+        <img src={logo} alt="Logo" />
+        <h1>Coming Soon</h1>
+        <h3>Medicamento Site Is Nearly Ready</h3>
+        <div class="progress-bar">
+          <span></span>
+        </div>
+        <div class="sm">
+          <a href="https://www.facebook.com/Medicamento.eg/">
+            {/* <i class="fab fa-facebook-f"></i> */}
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <a href="https://mail.google.com/mail/?view=cm&fs=1&to=kamalmahainy@gmail.com&su=SUBJECT&body=BODY&bcc=someone.else@example.com">
+            <i class="fa fa-envelope" aria-hidden="true"></i>
+          </a>
+          {/* <a href="#">
+            <i class="fab fa-instagram"></i>
+          </a> */}
+          {/* <a href="#">
+            <i class="fab fa-twitter"></i>
+          </a> */}
+        </div>
+      </div>
     </div>
   );
 }
